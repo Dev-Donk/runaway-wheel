6b17bccca11fe44b9a6420713d8f1e1acbc9bded
Donk <dante.samarco@gmail.com>
2026-02-23 12:51:39 -0500

    Background Scroll
    Set up background scroll, and game difficulty variable to alter speed. Placeholder textures are in place.

--- src/main.c
+++ src/main.c
@@ -19,28 +19,67 @@ int main ()
 	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 
 	// Create the window and OpenGL context
-	InitWindow(1280, 800, "Runaway Wheel");
+	// Size set so textures are tilable
+	InitWindow(2048, 1024, "Runaway Wheel");
 
 	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
 	SearchAndSetResourceDir("resources");
 
-	// Load a texture from the resources directory
-	Texture wabbit = LoadTexture("wabbit_alpha.png");
+	// Load textures
+	Texture game_foreground = LoadTexture("foreground_test.png");
+	Texture game_midground = LoadTexture("midground_test.png");
+	Texture game_background = LoadTexture("background_test.png");
+
+	// Background speeds
+	float scrolling_front 	= 0.0f;
+	float scrolling_mid 	= 0.0f;
+	float scrolling_back 	= 0.0f;
+
+	// Game Difficulty
+	float game_difficulty = 1.0f;
+
+	SetTargetFPS(60); // I mean.. It's a small game, most Windows devices should support
 	
 	// game loop
 	while (!WindowShouldClose())		// run the loop until the user presses ESCAPE or presses the Close button on the window
 	{
-		// drawing
+		// Update
+		// --------------------------------------------------------------
+		scrolling_back 	-= 1.0f * game_difficulty;
+		scrolling_mid 	-= 1.5f * game_difficulty;
+		scrolling_front -= 3.0f * game_difficulty;
+
+		// Textures is scaled twice it's size
+		if (scrolling_front <= -game_foreground.width * 2) scrolling_front = 0;
+		if (scrolling_mid <= -game_midground.width * 2) scrolling_mid = 0;
+		if (scrolling_back <= -game_background.width * 2) scrolling_back = 0;
+
+		// --------------------------------------------------------------
+
+		// Drawing
+		// --------------------------------------------------------------
 		BeginDrawing();
 
 		// Setup the back buffer for drawing (clear color and depth buffers)
 		ClearBackground(BLACK);
 
-		// draw some text using the default font
-		DrawText("Hello Raylib", 200,200,20,WHITE);
+		// World setting
+		// Background
+		DrawTextureEx(game_background, (Vector2){ scrolling_back, 0 }, 0.0f, 2.0f, WHITE);
+		DrawTextureEx(game_background, (Vector2){ game_background.width * 2 + scrolling_back, 0 }, 0.0f, 2.0f, WHITE);
+		// Background
+		DrawTextureEx(game_midground, (Vector2){ scrolling_mid, 0 }, 0.0f, 2.0f, WHITE);
+		DrawTextureEx(game_midground, (Vector2){ game_midground.width * 2 + scrolling_mid, 0 }, 0.0f, 2.0f, WHITE);
+		// Foregorund
+		DrawTextureEx(game_foreground, (Vector2){ scrolling_front, 0 }, 0.0f, 2.0f, WHITE);
+		DrawTextureEx(game_foreground, (Vector2){ game_foreground.width * 2 + scrolling_front, 0 }, 0.0f, 2.0f, WHITE);
 
-		// draw our texture to the screen
-		DrawTexture(wabbit, 400, 200, WHITE);
+		
+		// Text
+		//DrawText("Hello Raylib", 200,200,20,WHITE);
+
+		// --------------------------------------------------------------
+
 		
 		// end the frame and get ready for the next one  (display frame, poll input, etc...)
 		EndDrawing();
@@ -48,7 +87,9 @@ int main ()
 
 	// cleanup
 	// unload our texture so it can be cleaned up
-	UnloadTexture(wabbit);
+	UnloadTexture(game_foreground);
+	UnloadTexture(game_midground);
+	UnloadTexture(game_background);
 
 	// destroy the window and cleanup the OpenGL context
 	CloseWindow();
